2323741b399e0b1fc03f24adaf20d566eea296f9
Aiden Thacker <aidenthacker86@gmail.com>
2024-05-02 17:35:45 -0500

    We have Enemy Scale and Rotation now

--- app/src/main/java/com/example/mobileappfinal/EnemyShip.kt
+++ app/src/main/java/com/example/mobileappfinal/EnemyShip.kt
@@ -3,37 +3,61 @@ package com.example.mobileappfinal
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import java.util.Random
 
 class EnemyShip(private val context: Context)
 {
-    private val enemySpaceship: Bitmap
+    private val originalEnemySpaceship: Bitmap
+    private lateinit var scaledEnemySpaceship: Bitmap
     var x: Int = 0
     var y: Int = 0
     var enemyVelocity: Int = 0
     private val random: Random
 
-    init
-    {
-        enemySpaceship = BitmapFactory.decodeResource(context.resources, R.drawable.shipsidesfive)
+    //Spawns the ship in
+    init {
+        originalEnemySpaceship = BitmapFactory.decodeResource(context.resources, R.drawable.shipsidesfive)
         random = Random()
         x = 200 + random.nextInt(400)
         y = 0
         enemyVelocity = 14 + random.nextInt(10)
+        scaleBitmap()
+        rotateBitmap()
+    }
+    //Scaling the image on of the AI
+    private fun scaleBitmap()
+    {
+        val scaleFactor = 1.5f
+        scaledEnemySpaceship = Bitmap.createScaledBitmap(originalEnemySpaceship,
+            (originalEnemySpaceship.width.toFloat() * scaleFactor).toInt(),
+            (originalEnemySpaceship.height.toFloat() * scaleFactor).toInt(), false)
     }
 
+    //Rotates the image to face Player
+    private fun rotateBitmap()
+    {
+        val matrix = Matrix()
+        matrix.postRotate(180f)
+
+        scaledEnemySpaceship = Bitmap.createBitmap(scaledEnemySpaceship, 0, 0, scaledEnemySpaceship.width, scaledEnemySpaceship.height, matrix, true)
+    }
+
+    //Returns the scale of the AI image
     fun getEnemySpaceship(): Bitmap
     {
-        return enemySpaceship
+        return scaledEnemySpaceship
     }
 
+    //Returns the width of the AI image
     fun getEnemySpaceshipWidth(): Int
     {
-        return enemySpaceship.width
+        return scaledEnemySpaceship.width
     }
 
+    //Returns the height of the AI image
     fun getEnemySpaceshipHeight(): Int
     {
-        return enemySpaceship.height
+        return scaledEnemySpaceship.height
     }
 }
