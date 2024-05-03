27aa1f875a5b7e66a177a28ab34a62b24e4df27f
Treazurep <treazurep89@gmail.com>
2024-05-03 11:59:19 -0500

    the player health in game ui

--- app/src/main/java/com/example/mobileappfinal/GameUI.kt
+++ app/src/main/java/com/example/mobileappfinal/GameUI.kt
@@ -1,54 +1,51 @@
 package com.example.mobileappfinal
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
-
-import androidx.appcompat.app.AppCompatActivity
-import android.app.Activity
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class GameUI : AppCompatActivity() {
-//An attempt to Draw score and background to Canvas
-    override fun onDraw(canvas: Canvas){
-        canvas.drawBitmap(Bitmap, 0, 0)
-        canvas.drawText("Score: ", 0, 0, scorePaint)
-
-
-
-
-
-
-
-    }
 
     private var paused : Boolean = false
     private val context: Context = applicationContext
     private var handler: Handler? = null
+    private var screenWidth: Int = 0
+    var screenHeight: Int = 0
+    private lateinit var shipHealth: Bitmap
     private var points = 0
     private var life = 3
 
-    fun playerHealth()
-    {
+    fun playerHealth() {
+
+        @SuppressLint("DrawAllocation")
+        fun onDraw(canvas: Canvas) {
+            //Draw the players health
+            shipHealth = BitmapFactory.decodeResource(context.resources, R.drawable.ship)
 
 
+            /*for (i in life downTo 1) {
+                canvas.drawBitmap(shipHealth, screenWidth - shipHealth.width * i, 0f, null)
+            }*/
 
+            if (life == 0) {
+                paused = true
+                handler = null
+                /*val intent = Intent(context, GameOver::class.java)
+                intent.putExtra("points", points)
+                context.startActivity(intent)
+                (context as Activity).finish() */
+            }
 
-        if (life == 0) {
-            paused = true
-            handler = null
-            val intent = Intent(context, GameOver::class.java)
-            intent.putExtra("points", points)
-            context.startActivity(intent)
-            (context as Activity).finish()
         }
 
     }
-
-
-
-
 }
+
+
 
 
 
