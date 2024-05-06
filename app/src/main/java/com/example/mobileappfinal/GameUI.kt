422a5d61bd4f9c29af5737308e79c5a97aab5152
Treazurep <treazurep89@gmail.com>
2024-05-06 16:34:08 -0500

    Game UI updated

--- app/src/main/java/com/example/mobileappfinal/GameUI.kt
+++ app/src/main/java/com/example/mobileappfinal/GameUI.kt
@@ -1,10 +1,13 @@
 package com.example.mobileappfinal
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,26 +21,35 @@ class GameUI : AppCompatActivity() {
     private lateinit var shipHealth: Bitmap
     private var points = 0
     private var life = 3
+    private val textSize = 80
+    private val scorePaint = Paint().apply {
+        color = Color.RED
+        textSize = textSize
+        textAlign = Paint.Align.LEFT
+    }
+
+
 
     fun playerHealth() {
 
         @SuppressLint("DrawAllocation")
         fun onDraw(canvas: Canvas) {
-            //Draw the players health
+            //Draw the players health and points
             shipHealth = BitmapFactory.decodeResource(context.resources, R.drawable.ship)
+            canvas.drawText("Pts: $points", 0f, textSize.toFloat(), scorePaint)
 
+            for (i in life downTo 1) {
+                canvas.drawBitmap(shipHealth, (screenWidth - shipHealth.width * i).toFloat(), 0f, null)
+            }
 
-            /*for (i in life downTo 1) {
-                canvas.drawBitmap(shipHealth, screenWidth - shipHealth.width * i, 0f, null)
-            }*/
-
+            //When the player loses all lives go to game over screen
             if (life == 0) {
                 paused = true
                 handler = null
-                /*val intent = Intent(context, GameOver::class.java)
+                //val intent = Intent(context, GameOver::class.java)
                 intent.putExtra("points", points)
                 context.startActivity(intent)
-                (context as Activity).finish() */
+                (context as Activity).finish()
             }
 
         }
